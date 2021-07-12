f3d7f0e54d9252c0ec211e5d8bd5b36b0c9b9e0e
sigismundo4321 <sigismundo4321@gmail.com>
2021-07-12 11:47:15 -0300

    criando home

---
+++ lib/app/src/home/home_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  _HomeViwerState createState() => _HomeViwerState();
+}
+
+class _HomeViwerState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Lojas de carros"),
+        centerTitle: true,
+      ),
+    );
+  }
+}
