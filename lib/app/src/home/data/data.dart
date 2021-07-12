1903b6a30005e8e5b3168eefad8450dc3972b1a2
sigismundo4321 <sigismundo4321@gmail.com>
2021-07-12 16:38:12 -0300

    colocando os dados estatico

---
+++ lib/app/src/home/data/data.dart
@@ -0,0 +1,102 @@
+List<Map<String, dynamic>> produtos = [
+  {
+    "nome": "BMW",
+    "km": "",
+    "ano": "2017",
+    "cumbustivel": "alcool",
+    "preco": "240345",
+    "imagemURL":
+        "https://cdn.pixabay.com/photo/2015/09/02/12/25/bmw-918408_960_720.jpg",
+    "imagem3D": "",
+  },
+  {
+    "nome": "Porsche",
+    "km": "",
+    "ano": "2017",
+    "cumbustivel": "alcool",
+    "preco": "240345",
+    "imagemURL":
+        "https://cdn.pixabay.com/photo/2016/04/01/12/16/car-1300629_960_720.png",
+    "imagem3D": "",
+  },
+  {
+    "nome": "Lamborghini",
+    "km": "",
+    "ano": "2017",
+    "cumbustivel": "alcool",
+    "preco": "240345",
+    "imagemURL":
+        "https://cdn.pixabay.com/photo/2017/03/12/19/12/lamborghini-2137815_960_720.jpg",
+    "imagem3D": "",
+  },
+  {
+    "nome": "Maserati gran turismo",
+    "km": "",
+    "ano": "2017",
+    "cumbustivel": "alcool",
+    "preco": "240345",
+    "imagemURL":
+        "https://cdn.pixabay.com/photo/2016/09/06/13/37/maserati-gran-turismo-1649119_960_720.jpg",
+    "imagem3D": "",
+  },
+  {
+    "nome": "Mercedes",
+    "km": "",
+    "ano": "2017",
+    "cumbustivel": "alcool",
+    "preco": "240345",
+    "imagemURL":
+        "https://cdn.pixabay.com/photo/2016/05/03/23/46/mercedes-benz-1370536_960_720.jpg",
+    "imagem3D": "",
+  },
+  {
+    "nome": "BMW",
+    "km": "",
+    "ano": "2017",
+    "cumbustivel": "alcool",
+    "preco": "240345",
+    "imagemURL":
+        "https://cdn.pixabay.com/photo/2015/09/02/12/25/bmw-918408_960_720.jpg",
+    "imagem3D": "",
+  },
+  {
+    "nome": "Porsche",
+    "km": "",
+    "ano": "2017",
+    "cumbustivel": "alcool",
+    "preco": "240345",
+    "imagemURL":
+        "https://cdn.pixabay.com/photo/2016/04/01/12/16/car-1300629_960_720.png",
+    "imagem3D": "",
+  },
+  {
+    "nome": "Lamborghini",
+    "km": "",
+    "ano": "2017",
+    "cumbustivel": "alcool",
+    "preco": "240345",
+    "imagemURL":
+        "https://cdn.pixabay.com/photo/2017/03/12/19/12/lamborghini-2137815_960_720.jpg",
+    "imagem3D": "",
+  },
+  {
+    "nome": "Maserati gran turismo",
+    "km": "",
+    "ano": "2017",
+    "cumbustivel": "alcool",
+    "preco": "240345",
+    "imagemURL":
+        "https://cdn.pixabay.com/photo/2016/09/06/13/37/maserati-gran-turismo-1649119_960_720.jpg",
+    "imagem3D": "",
+  },
+  {
+    "nome": "Mercedes",
+    "km": "",
+    "ano": "2017",
+    "cumbustivel": "alcool",
+    "preco": "240345",
+    "imagemURL":
+        "https://cdn.pixabay.com/photo/2016/05/03/23/46/mercedes-benz-1370536_960_720.jpg",
+    "imagem3D": "",
+  },
+];
