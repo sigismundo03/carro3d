18d0dd3f317fba81cfdb7c6f278eef32ee71185c
sigismundo4321 <sigismundo4321@gmail.com>
2021-07-12 13:28:48 -0300

    criando home

--- lib/app/src/home/home_view.dart
+++ lib/app/src/home/home_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/card_carro.dart';
+
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
 
@@ -15,28 +17,7 @@ class _HomeViwerState extends State<HomeView> {
         title: const Text("Lojas de carros"),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 24,
-            ),
-            Container(
-              padding: const EdgeInsets.all(16),
-              height: 180,
-              width: 160,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: Image.network(
-                  "https://cdn.pixabay.com/photo/2015/09/02/12/25/bmw-918408_960_720.jpg"),
-            ),
-          ],
-        ),
-      ),
+      body: const CardCarro(),
     );
   }
 }
