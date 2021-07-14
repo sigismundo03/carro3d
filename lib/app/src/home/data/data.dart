5b975c10bd655faba9dfd602b5c88540841ebd74
sigismundo4321 <sigismundo4321@gmail.com>
2021-07-14 10:25:25 -0300

    atualizando os dados

--- lib/app/src/home/data/data.dart
+++ lib/app/src/home/data/data.dart
@@ -1,9 +1,9 @@
 List<Map<String, dynamic>> produtos = [
   {
     "nome": "BMW",
-    "km": "",
-    "ano": "2017",
-    "cumbustivel": "alcool",
+    "km": "0",
+    "ano": "2020",
+    "cumbustivel": "Flex(álcool/gasolina)",
     "preco": "240345",
     "imagemURL":
         "https://cdn.pixabay.com/photo/2015/09/02/12/25/bmw-918408_960_720.jpg",
@@ -11,9 +11,9 @@ List<Map<String, dynamic>> produtos = [
   },
   {
     "nome": "Porsche",
-    "km": "",
-    "ano": "2017",
-    "cumbustivel": "alcool",
+    "km": "0",
+    "ano": "2021",
+    "cumbustivel": "Flex(álcool/gasolina)",
     "preco": "240345",
     "imagemURL":
         "https://cdn.pixabay.com/photo/2016/04/01/12/16/car-1300629_960_720.png",
@@ -21,9 +21,9 @@ List<Map<String, dynamic>> produtos = [
   },
   {
     "nome": "Lamborghini",
-    "km": "",
-    "ano": "2017",
-    "cumbustivel": "alcool",
+    "km": "0",
+    "ano": "2021",
+    "cumbustivel": "Flex(álcool/gasolina)",
     "preco": "240345",
     "imagemURL":
         "https://cdn.pixabay.com/photo/2017/03/12/19/12/lamborghini-2137815_960_720.jpg",
@@ -31,29 +31,29 @@ List<Map<String, dynamic>> produtos = [
   },
   {
     "nome": "Maserati gran turismo",
-    "km": "",
-    "ano": "2017",
-    "cumbustivel": "alcool",
-    "preco": "240345",
+    "km": "0",
+    "ano": "2021",
+    "cumbustivel": "Flex(álcool/gasolina)",
+    "preco": "24345",
     "imagemURL":
         "https://cdn.pixabay.com/photo/2016/09/06/13/37/maserati-gran-turismo-1649119_960_720.jpg",
     "imagem3D": "",
   },
   {
     "nome": "Mercedes",
-    "km": "",
-    "ano": "2017",
-    "cumbustivel": "alcool",
-    "preco": "240345",
+    "km": "0",
+    "ano": "2019",
+    "cumbustivel": "Flex(álcool/gasolina)",
+    "preco": "20345",
     "imagemURL":
         "https://cdn.pixabay.com/photo/2016/05/03/23/46/mercedes-benz-1370536_960_720.jpg",
     "imagem3D": "",
   },
   {
     "nome": "BMW",
-    "km": "",
-    "ano": "2017",
-    "cumbustivel": "alcool",
+    "km": "0",
+    "ano": "2021",
+    "cumbustivel": "Flex(álcool/gasolina)",
     "preco": "240345",
     "imagemURL":
         "https://cdn.pixabay.com/photo/2015/09/02/12/25/bmw-918408_960_720.jpg",
@@ -61,9 +61,9 @@ List<Map<String, dynamic>> produtos = [
   },
   {
     "nome": "Porsche",
-    "km": "",
-    "ano": "2017",
-    "cumbustivel": "alcool",
+    "km": "0",
+    "ano": "2022",
+    "cumbustivel": "Flex(álcool/gasolina)",
     "preco": "240345",
     "imagemURL":
         "https://cdn.pixabay.com/photo/2016/04/01/12/16/car-1300629_960_720.png",
@@ -71,9 +71,9 @@ List<Map<String, dynamic>> produtos = [
   },
   {
     "nome": "Lamborghini",
-    "km": "",
-    "ano": "2017",
-    "cumbustivel": "alcool",
+    "km": "0",
+    "ano": "2020",
+    "cumbustivel": "Flex(álcool/gasolina)",
     "preco": "240345",
     "imagemURL":
         "https://cdn.pixabay.com/photo/2017/03/12/19/12/lamborghini-2137815_960_720.jpg",
@@ -81,9 +81,9 @@ List<Map<String, dynamic>> produtos = [
   },
   {
     "nome": "Maserati gran turismo",
-    "km": "",
-    "ano": "2017",
-    "cumbustivel": "alcool",
+    "km": "0",
+    "ano": "2020",
+    "cumbustivel": "Flex(álcool/gasolina)",
     "preco": "240345",
     "imagemURL":
         "https://cdn.pixabay.com/photo/2016/09/06/13/37/maserati-gran-turismo-1649119_960_720.jpg",
@@ -91,9 +91,9 @@ List<Map<String, dynamic>> produtos = [
   },
   {
     "nome": "Mercedes",
-    "km": "",
-    "ano": "2017",
-    "cumbustivel": "alcool",
+    "km": "0",
+    "ano": "2019",
+    "cumbustivel": "Flex(álcool/gasolina)",
     "preco": "240345",
     "imagemURL":
         "https://cdn.pixabay.com/photo/2016/05/03/23/46/mercedes-benz-1370536_960_720.jpg",
