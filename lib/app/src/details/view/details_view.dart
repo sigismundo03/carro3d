d0fadef69c8e0e8040d8973136e23ff0e6de2b47
sigismundo4321 <sigismundo4321@gmail.com>
2021-07-14 13:31:42 -0300

    colocando icon para abrir o modal

--- lib/app/src/details/view/details_view.dart
+++ lib/app/src/details/view/details_view.dart
@@ -1,3 +1,4 @@
+import 'package:carro3d/app/src/details/widgets/carddetalhe.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -23,15 +24,33 @@ class DetalisView extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Hero(
-              tag: item,
-              child: Container(
-                decoration: BoxDecoration(
-                  // shape: BoxShape.circle,
-                  borderRadius: BorderRadius.circular(16),
+            Stack(
+              children: [
+                Hero(
+                  tag: item,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      // shape: BoxShape.circle,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Image.network(item["imagemURL"]),
+                  ),
                 ),
-                child: Image.network(item["imagemURL"]),
-              ),
+                Container(
+                  height: 20,
+                  margin: const EdgeInsets.only(
+                    left: 360,
+                  ),
+                  child: const Tooltip(
+                    message: "Modelo 3D",
+                    child: Icon(
+                      FontAwesomeIcons.eye,
+                      color: Colors.white,
+                      size: 25,
+                    ),
+                  ),
+                ),
+              ],
             ),
             const SizedBox(
               height: 16,
@@ -47,6 +66,7 @@ class DetalisView extends StatelessWidget {
             //     style: const TextStyle(fontSize: 24),
             //   ),
             // ),
+
             Container(
               height: 200,
               margin: const EdgeInsets.only(top: 20),
@@ -74,42 +94,6 @@ class DetalisView extends StatelessWidget {
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class CardDetalhe extends StatelessWidget {
-  final String name;
-  final IconData icons;
-  const CardDetalhe({
-    Key? key,
-    required this.name,
-    required this.icons,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(left: 15),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(25),
-      ),
-      width: 160,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Icon(
-            icons,
-            color: Colors.amber,
-            size: 34,
-          ),
-          Text(
-            name,
-            style: const TextStyle(color: Colors.amber, fontSize: 20),
-          )
-        ],
       ),
     );
   }
