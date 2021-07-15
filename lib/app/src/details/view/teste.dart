b14419582696bcf6866701823eb0bd13ae1002d6
sigismundo4321 <sigismundo4321@gmail.com>
2021-07-15 09:20:54 -0300

    testando flutter_cube para modelo 3d

---
+++ lib/app/src/details/view/teste.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cube/flutter_cube.dart';
+
+class Teste extends StatefulWidget {
+  const Teste({Key? key}) : super(key: key);
+
+  @override
+  _TesteState createState() => _TesteState();
+}
+
+class _TesteState extends State<Teste> {
+  late Object shark;
+  @override
+  void initState() {
+    shark = Object(fileName: "assets/carro/teste.obj");
+    shark.rotation.setValues(0, 90, 0);
+    shark.updateTransform();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Cube(
+          onSceneCreated: (Scene scene) {
+            scene.world.add(shark);
+            scene.camera.zoom = 5;
+          },
+        ),
+      ),
+    );
+  }
+}
